8fc80ef279d301113d66d9f61b82980f451b8067
Dein GitHub-Benutzername <Deine GitHub-E-Mail-Adresse>
2023-06-14 10:06:01 +0200

    cvfgd

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -11,7 +13,7 @@ class UserController extends Controller
     {
         // Validation rules
         $request->validate([
-            'name' => 'required|min:4|string|max:255',
+            'name' => 'required|string|max:255',
             'email' => 'required|email|string|max:255',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
@@ -19,6 +21,30 @@ class UserController extends Controller
         $user = Auth::user();
         $user->name = $request->input('name');
         $user->email = $request->input('email');
+
+
+        if ($request->has('passwordOrigin') && $request->has('password') && $request->has('passwordRepeat')) {
+            $passwordOrigin = $request->input('passwordOrigin');
+            $password = $request->input('password');
+            $passwordRepeat = $request->input('passwordRepeat');
+
+            if (Hash::check($passwordOrigin, $user->password)) {
+                
+                if($password !== null && strlen($password) >=8 && $passwordRepeat !== null && strlen($passwordRepeat) >= 8 ){
+
+                    if ($password === $passwordRepeat) {
+                        $user->password = Hash::make($password);
+                    } else {
+                        return response()->json(['message' => 'Die Passwörter stimmen nicht überein'], 422);
+                    }
+                }
+                else{
+                        return response()->json(['message' => 'Die Passwörter müssen min 8 Zeichen lang sein'], 422);
+                }
+            } else {
+                return response()->json(['message' => 'Das aktuelle Passwort ist nicht korrekt'], 422);
+            }
+        } 
 
         if ($request->hasFile('image')) {
             $image = $request->file('image');
--- resources/js/components/Profil.vue
+++ resources/js/components/Profil.vue
@@ -3,24 +3,37 @@
     <div class="max-w-lg bg-black shadow-lg p-8 rounded-lg flex flex-col items-center">
       
       <div v-if="user">
-        <div class="mb-4 flex flex-col items-center">
-          <h1 class="text-2xl font-bold mb-4 text-white">Benutzerdetails</h1>
+        <div class="mb-2 flex flex-col items-center">
+          <h1 class="text-2xl font-bold mb-2 text-white">Benutzerdetails</h1>
           <img :src="selectedImage ? selectedImage : '/image/' + user.image" alt="Benutzerbild" ref="file" class="rounded-full mb-4" @click="selectImage" :style="{ cursor: selectedImage ? 'pointer' : 'default' }">
         </div>
-        <div class="mb-4 flex flex-col items-center">
+        <div class="mb-2 flex flex-col items-center">
           <label for="name" class="mb-1 text-lg text-white">Name:</label>
           <input type="text" id="name" v-model="user.name" class="border border-gray-300 rounded-lg mx-2 px-2 py-1 mb-2 text-center text-lg">
         </div>
-        <div class="mb-4 flex flex-col items-center">
+        <div class="mb-2 flex flex-col items-center">
           <label for="email" class="mb-1 text-lg text-white">E-Mail:</label>
           <input type="email" id="email" v-model="user.email" class="border border-gray-300 rounded-lg mx-2 px-2 py-1 text-center text-lg">
+        </div>
+        <div id="passwordcontainer" v-if="showPasswordPopup" class="mb-2 flex flex-col items-center">
+          <label for="passwordOrigin" class="mb-1 text-lg text-white">Aktuelles Passwort:</label>
+          <input type="password" id="passwordOrigin" v-model="user.passwordOrigin" class="border border-gray-300 rounded-lg mx-2 px-2 py-1 text-center text-lg">
+        </div>
+        <div id="passwordcontainer" v-if="showPasswordPopup" class="mb-2 flex flex-col items-center">
+          <label for="password" class="mb-1 text-lg text-white">Neues Passwort:</label>
+          <input type="password" id="password" v-model="user.password" class="border border-gray-300 rounded-lg mx-2 px-2 py-1 text-center text-lg">
+        </div>
+        <div id="passwordcontainer" v-if="showPasswordPopup" class="mb-2 flex flex-col items-center">
+          <label for="passwordRepeat" class="mb-1 text-lg text-white">Passwort wiederholen:</label>
+          <input type="password" id="passwordRepeat" v-model="user.passwordRepeat" class="border border-gray-300 rounded-lg mx-2 px-2 py-1 text-center text-lg">
         </div>
         <form @submit.prevent="submitUser" class="flex flex-col items-center">
           <input ref="fileInput" type="file" style="display: none" @change="handleImageChange" class="border border-gray-300 rounded-lg px-2 py-1">
           <button type="submit" class="bg-orange-500 text-black px-4 py-2 rounded-lg text-lg font-bold save-button">Speichern</button>
         </form>
-        <div class="mb-4 flex flex-col items-center">
-          <button class="bg-orange-500 text-black px-2 py-1 rounded-lg text-md font-bold mt-4 save-button" @click="redirectToHome">Back</button>
+        <div class="mb-4 flex flex-row items-center justify-center">
+          <button class="bg-orange-500 text-black mx-2 px-2 py-1 rounded-lg text-md font-bold mt-4 save-button" @click="togglePasswordContainers">Passwort ändern</button>
+          <button class="bg-orange-500 text-black mx-2 px-2 py-1 rounded-lg text-md font-bold mt-4 save-button" @click="redirectToHome">Back</button>
         </div>
       </div>
       <div v-else>
@@ -38,13 +51,22 @@ export default {
   data() {
     return {
       user: null,
-      selectedImage: null
+      selectedImage: null,
+      showPasswordPopup: false
     };
   },
   mounted() {
     this.fetchUser();
   },
   methods: {
+    togglePasswordContainers() {
+      this.showPasswordPopup = !this.showPasswordPopup;
+      if (this.showPasswordPopup === false) {
+        this.user.passwordRepeat = '';
+        this.user.password = '';
+        this.user.passwordOrigin = '';
+  }
+},
     redirectToHome() {
       window.location.href = '/';
     },
@@ -69,6 +91,11 @@ export default {
       const formData = new FormData();
       formData.append('name', this.user.name);
       formData.append('email', this.user.email);
+      if (this.showPasswordPopup === true) {
+        formData.append('password', this.user.password);
+        formData.append('passwordRepeat', this.user.passwordRepeat);
+        formData.append('passwordOrigin', this.user.passwordOrigin);
+      }
       if (this.$refs.fileInput.files[0]) {
         formData.append('image', this.$refs.fileInput.files[0]);
       }
@@ -81,7 +108,7 @@ export default {
         })
         .catch(error => {
           // Fehler verarbeiten
-          console.error(error);
+          console.error(error.response.data);
         });
     }
   }
